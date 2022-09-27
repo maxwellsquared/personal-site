ca2d1c20ddc7ad4a629b008fbbe8bdb22fb878f9
max k <maxkuhn@gmail.com>
2022-09-27 14:56:09 -0700

    Add camera operator and textures

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -24,12 +26,41 @@ const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
 const pointLight = new THREE.PointLight(0xFFFFFF);
-pointLight.position.set(25, 25, 25);
+pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0x555555);
 scene.add(pointLight, ambientLight);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(lightHelper);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('./images/space-background.jpg');
+scene.background = spaceTexture;
+
+// avatar
+
+const maxTexture = new THREE.TextureLoader().load('./images/maxkuhn.jpg');
+const max = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: maxTexture })
+);
+
+scene.add(max);
 
 function animate() {
   // game loop!
@@ -38,6 +69,9 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.002;
   torus.rotation.z += 0.03;
+
+  controls.update();
+
   renderer.render(scene, camera);
 }
 
